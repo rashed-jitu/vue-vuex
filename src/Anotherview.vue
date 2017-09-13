7d08a21d94bf04210942904f03aa98b21c444a1a
Rashedur Rahman <rashedur.rahman@sekai-lab-bd.com>
2017-09-13 17:15:08 +0600

    getters and three(...) pakage added

---
+++ src/Anotherview.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="current-value">
+        Current Counter: {{ doubleCounter }}
+        <br>
+        Clicks : {{ stringCounter }}
+    </div>
+</template>
+<script>
+    import { mapGetters } from 'vuex';
+    import {EventBus} from './main';
+    export default{
+        data(){
+            return {
+
+            }
+        },
+        computed: {
+            ...mapGetters(['doubleCounter','stringCounter']),
+        }
+//        created(){
+//
+//            EventBus.$on('counterData',(counterData)=>{
+//                this.counter = counterData
+////            });
+//        }
+    }
+</script>
