fc52d616bb953ef569eabcd3ceca5a2379146d3f
Rashedur Rahman <rashedur.rahman@sekai-lab-bd.com>
2017-09-13 17:29:03 +0600

    Done with three dot notation

--- src/actionButton.vue
+++ src/actionButton.vue
@@ -1,26 +1,14 @@
 <template>
     <div class="action-buttons">
-        <button @click="counter++;doAction(true);">Increment <span>+</span></button>
-        <button @click="counter--;doAction(false);">Decrement <span>-</span></button>
+        <button @click="increment">Increment <span>+</span></button>
+        <button @click="decrement">Decrement <span>-</span></button>
     </div>
 </template>
 <script>
-    import { EventBus } from './main';
+    import { mapMutations } from 'vuex';
     export default{
-        data(){
-            return {
-                counter : 0
-            }
-        },
         methods:{
-            doAction(isIncrement){
-                if(isIncrement){
-                    this.$store.commit('increment');
-                }else{
-                    this.$store.commit('decrement');
-                }
-//                EventBus.$emit('counterData',this.counter)
-            }
+            ...mapMutations(['increment','decrement']),
         }
     }
 </script>
